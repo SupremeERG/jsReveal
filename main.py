6f61593d723aaa9bec5a8b6a1cff62268c823fcb
SupremeERG <ethang717163@icloud.com>
2023-11-02 23:24:46 -0400

    added some notes

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ with open("regex.txt", "r") as regular_expression_list:
     regular_expressions = regular_expression_list.readlines()
 
 print(len(regular_expressions))
+
+"""
+here, the file will get tested against all the regular expressions and findings will be outputted similar to js miner
+but we need a way to organize regex patterns into categories: example below
+    /secr(e|3)t=/ | CATEGORY secret/api key
+    /url=http:\/\/localhost | CATEGORY hidden url endpoint
+"""
