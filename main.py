92559a72640fac3422d7c70fb2ff87e439b3b228
Papurudoragon <81523151+Papurudoragon@users.noreply.github.com>
2023-11-03 02:13:28 -0400

    init update -pap

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 import sys
-
-with open(sys.argv[1], "r") as target_file:
-    js_code = target_file.read()
-
-with open("regex.txt", "r") as regular_expression_list:
-    regular_expressions = regular_expression_list.readlines()
-
-print(len(regular_expressions))
+import re
+import json
 
 """
 here, the file will get tested against all the regular expressions and findings will be outputted similar to js miner
@@ -15,3 +9,26 @@ but we need a way to organize regex patterns into categories: example below
     /secr(e|3)t=/ | CATEGORY secret/api key
     /url=http:\/\/localhost | CATEGORY hidden url endpoint
 """
+
+def parse_json():
+    # grab our json file
+    with open(sys.argv[1], "r") as target_file:
+        js_code = target_file.read()
+
+    # grab our regex
+    with open("regex.json", "r") as regex_file:
+        categories = json.load(regex_file)
+
+    for category, patterns in categories.items():
+        for pattern in patterns:
+            
+            #print our matching patterns
+            matches = re.findall(pattern, js_code)
+            if matches:
+                print(f"CATEGORY {category}: {', '.join(matches)}")
+
+def main():
+    parse_json()
+
+if __name__=="__main__":
+    main()
