c77eee53c8d91151fa4879af490ec3bd618bd38a
Papurudoragon <81523151+Papurudoragon@users.noreply.github.com>
2023-11-11 23:16:51 -0500

    added flags and update to parsing files

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 
 reveals interesting js code to ethical hackers (endpoints, keywords, credentials, encoded data, vulnerable code) IN THIS ORDER
+
+
+Usage: go run main.go -f <input_file.js> 
+
+-f - file with js code
+-l - file with links to js code
+-v - for verbose
--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 
 	// "strings"
@@ -46,11 +49,11 @@ func fetchPatterns() ([]string, error) {
 	return patterns, scanner.Err()
 }
 
-func parseJS() {
+func parseJS(jsFilePath string) {
 	var matchTest *regexp2.Match
 	var matches = []string{}
 
-	jsCode, err := os.ReadFile(os.Args[1])
+	jsCode, err := os.ReadFile(jsFilePath)
 	if err != nil {
 		fmt.Println("Error reading JS file:", err)
 		return
@@ -83,13 +86,114 @@ func parseJS() {
 	}
 }
 
+// parseJSFromCode parses JavaScript code from a string.
+func parseJSFromCode(jsCode string) {
+	var matchTest *regexp2.Match
+	var matches = []string{}
+
+	// Grab the regex patterns from the file
+	patterns, err := fetchPatterns()
+	if err != nil {
+		log.Fatal("Error reading regex patterns: ", err)
+	}
+
+	regexProperties := RegexProperties{MatchLine: false, CaseInsensitive: true} // Example properties
+
+	for _, pattern := range patterns {
+		regexpPattern, err := compilePattern(pattern, regexProperties)
+		if err != nil {
+			log.Fatal("Error compiling regular expression '", pattern, "': ", err)
+		}
+		matchTest, _ = regexpPattern.FindStringMatch(jsCode)
+
+		for matchTest != nil {
+			matches = append(matches, matchTest.String())
+			match := matchTest.String()
+			if len(match) > 1000 {
+				match = match[:250] + "\n" // Prevents large blocks of code
+			}
+			fmt.Printf("Pattern: %s\nMatch: %s\n\n", pattern, match)
+			matchTest, _ = regexpPattern.FindNextMatch(matchTest)
+		}
+	}
+}
+
+// function to read JS links from a file
+func readJSLinks(filePath string) ([]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var links []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		links = append(links, scanner.Text())
+	}
+
+	return links, scanner.Err()
+}
+
+// Function to fetch JS code from a URL
+func fetchJSFromURL(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
+
 func main() {
-	log.SetFlags(0)
+	jsFilePath := flag.String("f", "", "Path to the .js file")
+	jsLinksPath := flag.String("l", "", "Path to the file with JS links")
+	verbose := flag.Bool("v", false, "Enable verbose output")
+	flag.Parse()
+
+	if *verbose {
+		log.SetFlags(log.LstdFlags | log.Lshortfile)
+	} else {
+		log.SetFlags(0)
+	}
+
 	log.SetPrefix("jsreveal: ")
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: go run main.go <input_file.js>")
+
+	// Check if the JS links file path is provided
+	if *jsLinksPath != "" {
+		if *verbose {
+			log.Println("Processing JS links from file:", *jsLinksPath)
+		}
+
+		links, err := readJSLinks(*jsLinksPath)
+		if err != nil {
+			log.Fatal("Error reading JS links: ", err)
+		}
+
+		for _, link := range links {
+			jsCode, err := fetchJSFromURL(link)
+			if err != nil {
+				log.Printf("Error fetching JS from URL %s: %v\n", link, err)
+				continue
+			}
+			parseJSFromCode(jsCode) // Assuming parseJSFromCode accepts a string of JS code
+		}
 		return
 	}
 
-	parseJS()
+	if *jsFilePath == "" {
+		fmt.Println("Usage: go run main.go -f <input_file.js> [-v for verbose]\n-f - file with js code\n-l - file with links to js code\n-v - for verbose")
+		return
+	}
+
+	if *verbose {
+		log.Println("Verbose mode enabled")
+	}
+	parseJS(*jsFilePath)
 }
