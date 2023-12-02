6fcc7f02871d6dfac6956a2cd4ae609ef421bb6c
SupremeERG <ethang717163@icloud.com>
2023-12-02 16:55:47 -0600

    Fixed deadlock error when no options given

--- main.go
+++ main.go
@@ -29,45 +29,57 @@ func readJSLinks(filePath string) ([]string, error) {
 	return links, scanner.Err()
 }
 
+func run(options runner.Options, outputChannel chan string, cleanPattern *regexp.Regexp) {
+	switch options.Source {
+	default:
+		//fmt.Println("./jsReveal -u <url to JS file>")
+		return
+	case 1:
+		parser.ParseJS(options.JSFilePath, options.Verbose, options.RegexFilePath, outputChannel)
+	case 2: // -l flag for a file containing multiple JS file paths
+		parser.ParseJSFromList(options.JSLinksPath, options.Verbose, options.RegexFilePath, outputChannel)
+	case 3:
+		if options.Verbose {
+			log.Println("Processing Code from " + cleanPattern.ReplaceAllString(options.JSURL, ""))
+		}
+		ch := make(chan string)
+		go fetchcode.FetchJSFromURL(options.JSURL, ch)
+		jsCode := <-ch
+		parser.ParseJSFromCode(jsCode, options.JSURL, options.Verbose, options.RegexFilePath, outputChannel)
+	}
+
+	return
+}
+
 func main() {
 	options := runner.ParseOptions()
 	outputChannel := make(chan string)
 	cleanPattern, _ := regexp.Compile(".*/")
-	go func() {
-		switch options.Source {
-		default:
-			fmt.Println("./jsReveal -u <url to JS file>")
-		case 1:
-			parser.ParseJS(options.JSFilePath, options.Verbose, options.RegexFilePath, outputChannel)
-		case 2: // -l flag for a file containing multiple JS file paths
-			parser.ParseJSFromList(options.JSLinksPath, options.Verbose, options.RegexFilePath, outputChannel)
-		case 3:
-			if options.Verbose {
-				log.Println("Processing Code from " + cleanPattern.ReplaceAllString(options.JSURL, ""))
-			}
-			ch := make(chan string)
-			go fetchcode.FetchJSFromURL(options.JSURL, ch)
-			jsCode := <-ch
-			parser.ParseJSFromCode(jsCode, options.JSURL, options.Verbose, options.RegexFilePath, outputChannel)
-		}
-	}()
 
-	// output functionality
-	if options.FileOutput != "" {
-		f, err := os.Create(options.FileOutput)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer f.Close()
-
-		w := bufio.NewWriter(f)
-		for output := range outputChannel {
-			fmt.Fprintln(w, output)
-			w.Flush()
-		}
+	// Checks if arguments were passed
+	if options.Source == 0 {
+		fmt.Println("./jsReveal -u <url to JS file>")
+		return
 	} else {
-		for output := range outputChannel {
-			fmt.Println(output)
+		go run(options, outputChannel, cleanPattern)
+
+		// Output Component
+		if options.FileOutput != "" {
+			f, err := os.Create(options.FileOutput)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer f.Close()
+
+			w := bufio.NewWriter(f)
+			for output := range outputChannel {
+				fmt.Fprintln(w, output)
+				w.Flush()
+			}
+		} else {
+			for output := range outputChannel {
+				fmt.Println(output)
+			}
 		}
 	}
 }
